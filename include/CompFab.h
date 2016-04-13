c81630669f840b1f7222c81e4953b66ec9081e21
Julian Contreras <jrcontre@mit.edu>
2016-04-12 23:46:49 -0400

    added setters

--- include/CompFab.h
+++ include/CompFab.h
@@ -130,10 +130,13 @@ namespace CompFab
             m_insideArray[k*(m_dimX*m_dimY)+j*m_dimY + i] = true;
         }      
 
-        inline bool & isOnSurface(unsigned int i, unsigned int j, unsigned int k)
-        {
+        inline bool & isOnSurface(unsigned int i, unsigned int j, unsigned int k) {
             return m_surfaceArray[k*(m_dimX*m_dimY)+j*m_dimY + i];
         }
+
+        inline void setOnSurface(unsigned int i, unsigned int j, unsigned int k) {
+            m_surfaceArray[k*(m_dimX*m_dimY)+j*m_dimY + i] = true;
+        }  
         bool *m_insideArray;
         bool *m_surfaceArray;
         unsigned int m_dimX, m_dimY, m_dimZ, m_size;
